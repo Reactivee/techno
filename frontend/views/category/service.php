33c79a75227f89edd4d0736b7d12be9c6cdd3aba
iD-House <idadajon@gmail.com>
2023-11-30 15:59:20 +0200

    asdфы

--- frontend/views/category/service.php
+++ frontend/views/category/service.php
@@ -15,7 +15,7 @@ $this->title = Yii::t('main', 'service');
 
 ?>
 <div class=" wow fadeIn position-relative" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
-    <img class="top_banner w-100" src="/uploads/banners/main_banner.png" alt="">
+    <img class="top_banner w-100" src="/uploads/banners/about.png" alt="">
     <div class="content_top_banner">
         <div class="container">
             <div class="row">
